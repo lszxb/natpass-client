e06bf9f4dd38d00780fd6040472a39cddd32e29a
lszxb <798971857.ab@gmail.com>
2017-06-06 16:54:50 +0000

    Fix syntax errors

--- natpassclient.py
+++ natpassclient.py
@@ -1,49 +1,54 @@
+#!/usr/bin/env python3
+
+
 import socket
 import sys
 
 defaultPort = 7989
 
-def connectToClient(myName, clientName, serverIP, serverPort=defaultPort, sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)):
+
+def ConnectToClient(myName, clientName, serverIP, serverPort=defaultPort,
+                    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)):
     data = 'request\n' + myName + '\n' + clientName
-    sock.sendto(data.encode('utf-8')， (serverIP, serverPort))
+    sock.sendto(data.encode('utf-8'), (serverIP, serverPort))
     clientIP = None
     clientPort = None
+    serverID = None
     while True:
-        respone = sock.recv(1024).decode('utf-8').split('\n')
-        if respone[0] == 'wait':
-            serverID = respone[1]
+        response = sock.recv(1024).decode('utf-8').split('\n')
+        if response[0] == 'wait':
+            serverID = response[1]
             data = 'done\n' + myName
-            sock.sendto(data.encode('utf-8')， (serverIP, serverPort)
-        elif respone[0] == 'send' and respone[-1] == serverID:
+            sock.sendto(data.encode('utf-8'), (serverIP, serverPort))
+        elif response[0] == 'send' and response[-1] == serverID:
             data = 'hello\n' + myName
-            clientIP = respone[1]
-            clientPort = int(respone[2])
-            sock.sendto(data.encode('utf-8')， (clientIP, clientPort))
+            clientIP = response[1]
+            clientPort = int(response[2])
+            sock.sendto(data.encode('utf-8'), (clientIP, clientPort))
             data = 'done\n' + myName
-            sock.sendto(data.encode('utf-8')， (serverIP, serverPort)
-        elif respone[0] == 'hello' and respone[-1] == clientName:
+            sock.sendto(data.encode('utf-8'), (serverIP, serverPort))
+        elif response[0] == 'hello' and response[-1] == clientName:
             data = 'welcome\n' + myName
             sock.sendto(data.encode('utf-8'), (clientIP, clientPort))
-        elif respone[0] == 'welcome' and respone[-1] == clientName:
+        elif response[0] == 'welcome' and response[-1] == clientName:
             data = 'sure\n' + myName
             sock.sendto(data.encode('utf-8'), (clientIP, clientPort))
             break
-        elif respone[0] == 'sure' and respone[-1] == clientName:
+        elif response[0] == 'sure' and response[-1] == clientName:
             data = 'success\n' + myName
-            sock.sendto(data.encode('utf-8')， (serverIP, serverPort)
+            sock.sendto(data.encode('utf-8'), (serverIP, serverPort))
             break
     return sock, (clientIP, clientPort)
 
 
-
 if __name__ == '__main__':
     if len(sys.argv) == 4:
-        if connectToClient(sys.argv[1], sys.argv[2], sys.argv[3]):
+        if ConnectToClient(sys.argv[1], sys.argv[2], sys.argv[3]):
             print('Success!')
         else:
             print('Error!')
     elif len(sys.argv) == 5:
-        if connectToClient(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4])):
+        if ConnectToClient(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4])):
             print('Success!')
         else:
             print('Error!')
